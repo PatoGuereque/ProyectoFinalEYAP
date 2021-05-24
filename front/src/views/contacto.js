8427abdf421aab1641e32c7618875b9f2369d081
Marcos Quintero <a00824603@itesm.mx>
2021-05-24 17:52:14 -0600

    pagina de contacto

--- front/src/views/contacto.js
+++ front/src/views/contacto.js
@@ -1,8 +1,43 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 400,
+    },
+  },
+}));
+
+
 const Contact = () => {
-  return <Typography>Contacto</Typography>;
+  const classes = useStyles();
+
+  return (
+    <div style={{padding: '10px', width: '400px'}}>
+      <Typography variant="h3">
+        Enviar un correo
+      </Typography>
+      <form className={classes.root}>
+        <div>
+          <TextField label="Nombre completo" fullWidth autocomplete="none"/>
+        </div>
+        <div>
+          <TextField label="Correo" fullWidth autocomplete="none"/>
+        </div>
+        <div>
+          <TextField label="Mensaje" fullWidth multiline rows={5} autoComplete="none" />
+        </div>
+        <Button type="submit" variant="contained" color="primary" style={{margin: '0 40%'}}>Enviar</Button>
+    </form>
+    </div>
+    
+  )
 };
 
 export default Contact;
